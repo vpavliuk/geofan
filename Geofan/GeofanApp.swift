4db1dcf43a178ee5ebd7ebb1c90ebb0e7485e882
Volodymyr Pavliuk <gwotan@gmail.com>
2024-09-07 16:26:03 +0200

    Create project

---
+++ Geofan/GeofanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofanApp.swift
+//  Geofan
+//
+//  Created by Vova on 07.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeofanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
